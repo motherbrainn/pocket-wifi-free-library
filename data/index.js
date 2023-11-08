62f00777930f205be50d07492dbc297357e930a3
Aaron Ehresman <aaron.ehr@gmail.com>
2023-11-07 20:59:24 -0800

    feat: add loading overlay

---
+++ data/index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  const anchorTags = document.querySelectorAll("a");
+
+  anchorTags.forEach(function (anchor) {
+    anchor.addEventListener("click", function (event) {
+      // Show the loading overlay
+      loadingOverlay.style.display = "block";
+
+      // Prevent the default behavior (page navigation)
+      event.preventDefault();
+
+      // Load the new page immediately
+      window.location = anchor.getAttribute("href");
+    });
+  });
+
+  // Detect when the new page has finished loading
+  window.onload = function () {
+    // Hide the loading overlay when the new page is fully loaded
+    loadingOverlay.style.display = "none";
+  };
+});
