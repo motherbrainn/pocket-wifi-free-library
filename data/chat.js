58f7e29f0745850da241956fd1bc4bcfa91f9e82
Aaron Ehresman <aaron.ehr@gmail.com>
2023-11-09 20:38:42 -0800

    feat: add basic chat

---
+++ data/chat.js
@@ -0,0 +1,84 @@
+let socket; // Declare socket variable globally
+let userId;
+
+function generateRandomUserId() {
+  return Math.random().toString(36).substr(2, 9); // Generate a random alphanumeric user ID
+}
+
+function showLoadingOverlay() {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  loadingOverlay.style.display = "block";
+}
+
+function hideLoadingOverlay() {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  loadingOverlay.style.display = "none";
+}
+
+function initWebSocket() {
+  showLoadingOverlay(); // Show loading overlay when WebSocket is opening
+  userId = generateRandomUserId(); // Generate a unique user ID
+  console.log("User ID:", userId);
+
+  socket = new WebSocket("ws://192.168.4.1:81");
+
+  socket.addEventListener("open", (event) => {
+    console.log("WebSocket connection opened:", event);
+    hideLoadingOverlay(); // Hide loading overlay when WebSocket is opened
+  });
+
+  socket.addEventListener("message", (event) => {
+    console.log("Message received:", event.data);
+    // Handle the received message
+    displayMessage(event.data);
+  });
+
+  socket.addEventListener("close", (event) => {
+    console.log("WebSocket connection closed:", event);
+  });
+
+  socket.addEventListener("error", (event) => {
+    console.error("WebSocket error:", event);
+    hideLoadingOverlay(); // Hide loading overlay in case of an error
+  });
+}
+
+function displayMessage(message) {
+  const chatOutput = document.getElementById("chat-output");
+  const messageObj = JSON.parse(message);
+
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add("message-container");
+
+  const userIdElement = document.createElement("span");
+  userIdElement.classList.add("user-id");
+  userIdElement.textContent = `User ${messageObj.userId}: `;
+  messageContainer.appendChild(userIdElement);
+
+  const messageElement = document.createElement("span");
+  messageElement.classList.add("message");
+  messageElement.textContent = messageObj.message;
+  messageContainer.appendChild(messageElement);
+
+  chatOutput.appendChild(messageContainer);
+}
+
+function sendMessage() {
+  const messageInput = document.getElementById("message-input");
+  const message = messageInput.value.trim();
+
+  if (message !== "") {
+    if (socket.readyState === WebSocket.OPEN) {
+      const messageWithUserId = { userId, message };
+      socket.send(JSON.stringify(messageWithUserId));
+      console.log("Message sent successfully:", messageWithUserId);
+    } else {
+      console.error("WebSocket not open. Unable to send message.");
+    }
+
+    messageInput.value = "";
+  }
+}
+
+// Initialize the WebSocket connection
+initWebSocket();
